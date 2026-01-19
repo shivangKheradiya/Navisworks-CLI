62107ae5bb4d89d471a026f3e414a2edce2cfe80
shivangKheradiya <shivangatul@gmail.com>
2026-01-19 19:37:29 +0100

    .Net OOP Convention Implemented

--- NavisworksCLI/NavisworksCLI/NavisworksCLI.cs
+++ NavisworksCLI/NavisworksCLI/NavisworksCLI.cs
@@ -14,90 +14,121 @@ namespace NavisworksCLI
 
         public bool IsGUI { get; set; } = true;
 
-        public void AppendFile(string FileFullPathWithExtension)
-        {
-            m_doc.AppendFile(FileFullPathWithExtension);
-        }
+        #region Public API
 
-        public void OpenNavisworks(string FileFullPathWithExtension)
+        public void OpenNavisworks(string filePath)
         {
             m_doc = new NavisworksAutomationAPI23.Document();
             m_state = m_doc.State();
             m_doc.Visible = IsGUI;
-            m_doc.OpenFile(FileFullPathWithExtension);
+            m_doc.OpenFile(filePath);
         }
 
-        public void SaveNWD(string FileFullPathWithExtension)
+        public void AppendFile(string filePath)
         {
-            m_doc.SaveAs(FileFullPathWithExtension);
+            m_doc.AppendFile(filePath);
         }
 
-        public void SetColour(byte R, byte G, byte B)
+        public void SaveNWD(string filePath)
         {
-            NavisworksIntegratedAPI23.InwLVec3f colorVec = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLVec3f);
-            colorVec.SetValue(R , G , B);
+            m_doc.SaveAs(filePath);
+        }
 
-            NavisworksIntegratedAPI23.InwOpSelection selection = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwOpSelection);
+        public void SetColour(byte r, byte g, byte b)
+        {
+            var colorVec = CreateObject<NavisworksIntegratedAPI23.InwLVec3f>(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLVec3f);
+
+            colorVec.SetValue(r, g, b);
+
+            m_state.OverrideColor(GetSelectionAll(), colorVec);
+        }
+
+        public void SetOrientation(double xRotation, double yRotation, double zRotation)
+        {
+            ApplyAxisRotation(1, 0, 0, xRotation);
+            ApplyAxisRotation(0, 1, 0, yRotation);
+            ApplyAxisRotation(0, 0, 1, zRotation);
+        }
+
+        public void SetPosition(double xPos, double yPos, double zPos)
+        {
+            var vec = CreateVector(xPos, yPos, zPos);
+            var transform = CreateTransform();
+
+            transform.MakeTranslation(vec);
+
+            OverrideTransform(transform);
+        }
+
+        public void SetScale(double xScale, double yScale, double zScale)
+        {
+            var vec = CreateVector(xScale, yScale, zScale);
+            var transform = CreateTransform();
+
+            transform.MakeScale(vec);
+
+            OverrideTransform(transform);
+        }
+
+        public void SetTransperancy(double transparency)
+        {
+            m_state.OverrideTransparency(GetSelectionAll(), transparency);
+        }
+
+        #endregion
+
+        #region Rotation Helpers
+
+        private void ApplyAxisRotation(double x, double y, double z, double angle)
+        {
+            var axis = CreateObject<NavisworksIntegratedAPI23.InwLUnitVec3f>(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLUnitVec3f);
+
+            var rotation = CreateObject<NavisworksIntegratedAPI23.InwLRotation3f>(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLRotation3f);
+
+            var transform = CreateTransform();
+
+            axis.SetValue(x, y, z);
+            rotation.SetValue(axis, angle);
+            transform.MakeRotation(rotation);
+
+            OverrideTransform(transform);
+        }
+
+        #endregion
+
+        #region Common Factory Helpers
+
+        private T CreateObject<T>(NavisworksIntegratedAPI23.nwEObjectType type)
+        {
+            return (T)m_state.ObjectFactory(type);
+        }
+
+        private NavisworksIntegratedAPI23.InwLTransform3f3 CreateTransform()
+        {
+            return CreateObject<NavisworksIntegratedAPI23.InwLTransform3f3>(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLTransform3f);
+        }
+
+        private NavisworksIntegratedAPI23.InwLVec3f CreateVector(double x, double y, double z)
+        {
+            var vec = CreateObject<NavisworksIntegratedAPI23.InwLVec3f>(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLVec3f);
+
+            vec.SetValue(x, y, z);
+            return vec;
+        }
+
+        private NavisworksIntegratedAPI23.InwOpSelection GetSelectionAll()
+        {
+            var selection = CreateObject<NavisworksIntegratedAPI23.InwOpSelection>(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwOpSelection);
+
             selection.SelectAll();
-
-            m_state.OverrideColor(selection, colorVec);
+            return selection;
         }
 
-        public void SetOrientation(double XRotation, double YRotation, double ZRotation)
+        private void OverrideTransform(NavisworksIntegratedAPI23.InwLTransform3f3 transform)
         {
-            ApplyAxisRotation(1, 0, 0, XRotation);
-            ApplyAxisRotation(0, 1, 0, YRotation);
-            ApplyAxisRotation(0, 0, 1, ZRotation);
+            m_state.OverrideTransform(GetSelectionAll(), transform);
         }
 
-        private void ApplyAxisRotation(double unitVectorX, double unitVectorY, double unitVectorZ, double angle)
-        {
-            NavisworksIntegratedAPI23.InwLTransform3f3 transVec = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLTransform3f);
-            NavisworksIntegratedAPI23.InwLRotation3f rotVal = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLRotation3f);
-            NavisworksIntegratedAPI23.InwLUnitVec3f Axis = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLUnitVec3f);
-
-            Axis.SetValue(unitVectorX, unitVectorY, unitVectorZ);
-            rotVal.SetValue(Axis, angle);
-            transVec.MakeRotation(rotVal);
-
-            NavisworksIntegratedAPI23.InwOpSelection selection = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwOpSelection);
-            selection.SelectAll();
-
-            m_state.OverrideTransform(selection, transVec);
-        }
-
-        public void SetPosition(double XPos, double YPos, double ZPos)
-        {
-            NavisworksIntegratedAPI23.InwLTransform3f3 transVec = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLTransform3f);
-            NavisworksIntegratedAPI23.InwLVec3f transVal = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLVec3f);
-
-            transVal.SetValue(XPos, YPos, ZPos);
-            transVec.MakeTranslation(transVal);
-
-            NavisworksIntegratedAPI23.InwOpSelection selection = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwOpSelection);
-            selection.SelectAll();
-
-            m_state.OverrideTransform(selection, transVec);
-        }
-
-        public void SetScale(double XScale, double YScale, double ZScale)
-        {
-            NavisworksIntegratedAPI23.InwLTransform3f3 transVec = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLTransform3f);
-            NavisworksIntegratedAPI23.InwLVec3f transVal = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwLVec3f);
-
-            transVal.SetValue(XScale, YScale, ZScale);
-            transVec.MakeScale(transVal);
-
-            NavisworksIntegratedAPI23.InwOpSelection selection = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwOpSelection);
-            selection.SelectAll();
-            m_state.OverrideTransform(selection, transVec);
-        }
-
-        public void SetTransperancy(double TransperancyNumber)
-        {
-            NavisworksIntegratedAPI23.InwOpSelection selection = m_state.ObjectFactory(NavisworksIntegratedAPI23.nwEObjectType.eObjectType_nwOpSelection);
-            selection.SelectAll();
-            m_state.OverrideTransparency(selection, TransperancyNumber);
-        }
+        #endregion
     }
 }
